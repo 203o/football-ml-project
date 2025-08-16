cc97f40613fa29a606777096bfbb91f9487e40bf
Herbert <Herberkom@gmail.com>
2025-08-16 03:06:58 +0300

    Auto update: 2025-08-16 03:06:58

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import os
 
+import joblib
+
+
+def load_model_and_scaler():
+    base_path = os.path.dirname(__file__)  # directory of predicts.py
+    nb_model = joblib.load(os.path.join(base_path, "nb_model.pkl"))
+    scaler = joblib.load(os.path.join(base_path, "scaler.pkl"))
+    return nb_model, scaler
+
 
 os.system("streamlit run models/predicts.py")
